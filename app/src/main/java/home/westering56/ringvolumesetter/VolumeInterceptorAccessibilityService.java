6108c2f529d8722d7f17a1a6b5d823ccc56fb6af
Andy Clark <ajc97@cantab.net>
2018-12-28 16:00:36 +0000

    Set ring volume from hardware
    Don't try and do anything fancy with detecting media playback.

--- app/src/main/java/home/westering56/ringvolumesetter/VolumeInterceptorAccessibilityService.java
+++ app/src/main/java/home/westering56/ringvolumesetter/VolumeInterceptorAccessibilityService.java
@@ -1,15 +1,17 @@
 package home.westering56.ringvolumesetter;
 
 import android.accessibilityservice.AccessibilityService;
-import android.app.Service;
 import android.content.Intent;
-import android.os.IBinder;
+import android.media.AudioManager;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
 public class VolumeInterceptorAccessibilityService extends AccessibilityService {
-    public static final String TAG = "VolumeIntAccI11ySvc";
+    private static final String TAG = "VolumeIntAccI11ySvc";
+    /** From AudioManager: Adjusting the volume due to a hardware key press. (hidden) */
+    private static final int HIDDEN_FLAG_FROM_KEY = 1 << 12;
+    private AudioManager audioManager;
 
     public VolumeInterceptorAccessibilityService() {
     }
@@ -17,30 +19,55 @@ public class VolumeInterceptorAccessibilityService extends AccessibilityService 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-        // TODO: Determine if any initialization is needed, do it here if it is.
-        Log.d(TAG, "OnServiceConnected");
+        Log.d(TAG, "onServiceConnected");
+        audioManager = getSystemService(AudioManager.class);
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.d(TAG, "Received accessibility event" + event.toString());
+        Log.e(TAG, "Received unexpected accessibility: event" + event.toString());
     }
 
     @Override
     public void onInterrupt() {
-        Log.d(TAG, "onInterrupt called");
+        Log.d(TAG, "onInterrupt called. No action.");
     }
 
     @Override
     public boolean onUnbind(Intent intent) {
-        // TODO: Determine if any teardown is needed, do it here if it is.
-        Log.d(TAG, "onUnbind: Intent="+intent.toString());
+        Log.d(TAG, "onUnbind: intent="+intent.toString());
+        audioManager = null;
         return super.onUnbind(intent);
     }
 
     @Override
     protected boolean onKeyEvent(KeyEvent event) {
-        Log.d(TAG, "Key event" + event.toString());
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP ||
+                event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            return handleVolUpDownEvent(event);
+        }
         return super.onKeyEvent(event);
+    }
+
+    private boolean handleVolUpDownEvent(final KeyEvent event) {
+        Log.d(TAG, "Vol up/down: event=" + event.toString());
+        final int keycode = event.getKeyCode();
+        final int direction;
+        if (keycode == KeyEvent.KEYCODE_VOLUME_UP) {
+            direction = AudioManager.ADJUST_RAISE;
+        } else if (keycode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            direction = AudioManager.ADJUST_LOWER;
+        } else {
+            Log.w(TAG, "Unable handle non-volume key event: " + event);
+            return false; // Allow other handlers to process this event
+        }
+        adjustVolume(direction);
+        return true; // swallow the event, we handled it
+    }
+
+    private void adjustVolume(final int direction) {
+        Log.d(TAG, "Adjusting volume: direction=" + direction);
+        audioManager.adjustStreamVolume(AudioManager.STREAM_RING, direction,
+                AudioManager.FLAG_SHOW_UI | HIDDEN_FLAG_FROM_KEY);
     }
 }
