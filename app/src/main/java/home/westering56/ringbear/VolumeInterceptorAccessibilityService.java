1648cc7bddda03e3a6a833b306523635b4863da1
Andy Clark <ajc97@cantab.net>
2018-12-28 17:39:00 +0000

    Act on key-up or on key-down for repeats
    Although the accessibility service doesn't seem to send us key
    repeat information...

--- app/src/main/java/home/westering56/ringbear/VolumeInterceptorAccessibilityService.java
+++ app/src/main/java/home/westering56/ringbear/VolumeInterceptorAccessibilityService.java
@@ -4,6 +4,7 @@ import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.KeyEvent;
 import android.view.accessibility.AccessibilityEvent;
 
@@ -11,6 +12,8 @@ public class VolumeInterceptorAccessibilityService extends AccessibilityService 
     private static final String TAG = "VolumeIntAccI11ySvc";
     /** From AudioManager: Adjusting the volume due to a hardware key press. (hidden) */
     private static final int HIDDEN_FLAG_FROM_KEY = 1 << 12;
+    /** Tracks the last action we saw of the volume keys we are interested in. a[keycode]=action. */
+    private SparseIntArray lastKeyAction;
     private AudioManager audioManager;
 
     public VolumeInterceptorAccessibilityService() {
@@ -21,6 +24,7 @@ public class VolumeInterceptorAccessibilityService extends AccessibilityService 
         super.onServiceConnected();
         Log.d(TAG, "onServiceConnected");
         audioManager = getSystemService(AudioManager.class);
+        lastKeyAction = new SparseIntArray(2);
     }
 
     @Override
@@ -37,6 +41,7 @@ public class VolumeInterceptorAccessibilityService extends AccessibilityService 
     public boolean onUnbind(Intent intent) {
         Log.d(TAG, "onUnbind: intent="+intent.toString());
         audioManager = null;
+        lastKeyAction = null;
         return super.onUnbind(intent);
     }
 
@@ -51,17 +56,23 @@ public class VolumeInterceptorAccessibilityService extends AccessibilityService 
 
     private boolean handleVolUpDownEvent(final KeyEvent event) {
         Log.d(TAG, "Vol up/down: event=" + event.toString());
-        final int keycode = event.getKeyCode();
         final int direction;
-        if (keycode == KeyEvent.KEYCODE_VOLUME_UP) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_UP) {
             direction = AudioManager.ADJUST_RAISE;
-        } else if (keycode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+        } else if (event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN) {
             direction = AudioManager.ADJUST_LOWER;
         } else {
-            Log.w(TAG, "Unable handle non-volume key event: " + event);
+            Log.w(TAG, "Unable handle unexpected non-volume key event: " + event);
             return false; // Allow other handlers to process this event
         }
-        adjustVolume(direction);
+
+        if (lastKeyAction.get(event.getKeyCode(), KeyEvent.ACTION_UP) == KeyEvent.ACTION_DOWN) {
+            /* take action on this event, as it's either an ACTION_UP (events normally take effect
+             * on up), or another in a series of ACTION_DOWN events which indicate key repeats. */
+            // A18y service doesn't seem to tell us about press-and-hold for key repeats though.
+            adjustVolume(direction);
+        }
+        lastKeyAction.put(event.getKeyCode(), event.getAction());
         return true; // swallow the event, we handled it
     }
 
